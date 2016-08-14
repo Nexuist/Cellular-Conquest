1c6e2fa1c83c8eaad6a67a08794d21fdd818cfe6
Nexuist <nexuister@gmail.com>
2016-08-14 08:20:57 -0400

    Added command line resolution setting

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 
 import pygame
 from src import Scenes
+import sys
 
 pygame.init()
 resolution = (1280, 800)
+if len(sys.argv) == 3:
+	resolution = (int(sys.argv[1]), int(sys.argv[2]))
 pygame.display.set_mode(resolution)
 pygame.display.set_caption("Cellular Conquest")
 Scenes.StartScene()
